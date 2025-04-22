41321240223991e461a27a620e5924479c4fd42b
prusotamydv <prusotamkumaryadav78@gmail.com>
2025-04-22 18:23:17 +0545

    login and register with hasing is completed

--- controllers/Auth.contoller.js
+++ controllers/Auth.contoller.js
@@ -1,3 +1,8 @@
+import UserModel from "../models/User.model.js";
+import bycrypt from "bcrypt";
+import { comparePassword, hashPassword } from "../utils/Auth.utils.js";
+const saltRounds = 10;
+
 ///LOGIN CONTOLLERS
 
 export const login = (req, res) => {
@@ -10,9 +15,20 @@ export const login = (req, res) => {
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await UserModel.findOne({ username, password });
+    const user = await UserModel.findOne({ username });
+    //checking the user is exsit or not
     if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res
+        .status(401)
+        .json({ message: "Invalid credentials, username not found" });
+    }
+
+    const isPasswordMatch = await comparePassword(password, user.password);
+    // comfirming the password
+    if (!isPasswordMatch) {
+      return res
+        .status(401)
+        .json({ message: "Invalid credentials, password is incorrect" });
     }
     res.status(200).json({ message: "Login successful" });
   } catch (error) {
@@ -30,11 +46,20 @@ export const register = (req, res) => {
 };
 
 export const registerUser = async (req, res) => {
+  const { username, email } = req.body;
+  const user = await UserModel.findOne({ username, email });
   try {
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    req.body.password = await hashPassword(req.body.password);
     const newUser = new UserModel(req.body);
     await newUser.save();
     res.status(201);
-    res.redirect("/");
+    res.json({
+      message: "user saved successfully",
+      user: newUser,
+    });
   } catch (error) {
     res.json({
       message: "faild to save user",
